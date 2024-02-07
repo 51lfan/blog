6389aa4f41366060bf886d62b24a47006b732020
51lfan <1973414293@qq.com>
2024-02-07 16:13:24 +0800

    first commit

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="footer has-text-centered">
-    <span>©LFan 2024. All rights reserved.</span>
+    <span>©51lfan 2024. All rights reserved.</span>
     <a href="http://www.miitbeian.gov.cn/" target="_blank">滇ICP备17004667号-1</a>
 </footer>
